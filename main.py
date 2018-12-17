7305a19090e3b795368783fa7b4b4e849c3294be
schonwetter <alexandre.bontems@gmail.com>
2018-12-17 16:17:46 +0100

    replace custom function with pandas+numpy

--- main.py
+++ main.py
@@ -1,67 +1,23 @@
-import math
-from copy import deepcopy
+from solver import *
 
-
-def augmented_tchebycheff_norm(point, reference, epsilon=0.001):
-	max_distance = 0
-	for category_idx in range(1, len(point)):
-		dist = reference[category_idx-1] -
-
-
-def get_ideal_point(X, directions):
-	ideal_point = [0 if dir == 'max' else math.inf for dir in directions]
-	for line in X:
-		for category_idx in range(len(ideal_point)):
-			if directions[category_idx] == 'max' and line[category_idx+1] > ideal_point[category_idx]:
-				ideal_point[category_idx] = line[category_idx+1]
-			if directions[category_idx] == 'min' and line[category_idx+1] < ideal_point[category_idx]:
-				ideal_point[category_idx] = line[category_idx+1]
-	return ideal_point
-
-
-def mean_wo_nan(x):
-	x = [i for i in x if not math.isnan(i)]
-	return sum(x) / len(x)
-
-
-def parse_data(filename):
-	X = list()
-	i = 0
-	design_nan = list()
-	frame_nan = list()
-
-	with open(filename, 'r') as f:
-		labels = f.readline()
-		data = f.read().split("\n")
-		for line in data:
-			linedata = line.strip().split(',')
-			X.append([linedata[0]] + list(map(float, linedata[1:])))
-
-			if math.isnan(X[i][-1]): frame_nan.append(i)
-			if math.isnan(X[i][-2]): design_nan.append(i)
-			i += 1
-
-	mean_frame = mean_wo_nan([row[-1] for row in X])
-	mean_design = mean_wo_nan([row[-2] for row in X])
-	for i in range(len(X)):
-		if i in frame_nan: X[i][-1] = mean_frame
-		if i in design_nan: X[i][-2] = mean_design
-
-	return X, labels
-
-
-def normalize(X):
-	X_copy = deepcopy(X)
-	for category in range(1, len(X_copy[0])):
-		values = list(line[category] for line in X_copy)
-		max_value = max(values)
-		min_value = min(values)
-		for line in X_copy:
-			line[category] = (line[category] - min_value) / (max_value - min_value)
-	return X_copy
 
 if __name__ == "__main__":
-	X, labels = parse_data('data.csv')
-	directions = ['max', 'max', 'min', 'min', 'min', 'min', 'max', 'max']
-	X_normalized = normalize(X)
-	print(get_ideal_point(X_normalized, directions))
+	columns_to_min = ['Weight', 'Acceleration', 'Price', 'Pollution']
+
+	# Read input data
+	X = pd.read_csv('data.csv', skiprows=1, dtype={'Design': np.float64, 'Frame': np.float64})
+	# Fill NaN values with mean
+	X.fillna(X.mean(), inplace=True)
+	# Numeric columns
+	columns = ['Engine', 'Torque', 'Weight', 'Acceleration', 'Price', 'Pollution', 'Design', 'Frame']
+	# min-max normalize
+	X_norm = (X[columns] - X[columns].min()) / (X[columns].max() - X[columns].min())
+	# Convert columns to minimize
+	X_norm[columns_to_min] = X_norm[columns_to_min].apply(lambda x: -x)
+	# Get pareto front
+	X_pareto = pareto_front(X_norm, use_cache=False)
+
+	ipt = get_ideal_point(X_pareto)
+	npt = get_nadir_point(X_pareto)
+
+
---
+++ solver.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+import math
+
+
+def _compute_pareto_front(X):
+	"""Computes the pareto front from a list of vectors to maximise.
+
+	Args:
+		X (pd.DataFrame): Input data.
+
+	Returns:
+		(pd.DataFrame) Pareto front of the input data.
+	"""
+	X_pareto = pd.DataFrame([])
+	dominates = {}
+	for i in range(len(X)):
+		for j in range(len(X)):
+			if i == j: continue
+			if np.all(X.iloc[i] >= X.iloc[j]) and np.any(X.iloc[i] > X.iloc[j]):
+				dominates[(i, j)] = True
+
+	for i in range(len(X)):
+		if not any([dominates.get((j, i), False) for j in range(len(X))]):
+			X_pareto = X_pareto.append(X.iloc[i])
+
+	return X_pareto
+
+
+def pareto_front(X, use_cache=True):
+	"""Computes the pareto front from a list of vectors to maximise. Uses
+	`pareto.csv` as a cache. If the file exists, its content is returned.
+
+	Args:
+		X (pd.DataFrame): Input data.
+		use_cache (bool): If false, force computation of pareto front and
+			saves it in `pareto.csv`.
+
+	Returns:
+		(pd.DataFrame) Pareto front of the input data.
+	"""
+	if use_cache:
+		try:
+			return pd.read_csv('pareto.csv', usecols=lambda i: i>=1)
+		except IOError:
+			print("No pareto file. Computing pareto front.")
+
+	X_pareto = _compute_pareto_front(X)
+	X_pareto.to_csv(path_or_buf='pareto.csv')
+	return X_pareto
+
+
+def get_ideal_point(X_p):
+	"""Returns the ideal point of `X_p`, that is the point that maximises each component
+
+	Args:
+		X_p (pd.DataFrame): Pareto front.
+
+	Returns: (pd.Series) Ideal point.
+	"""
+	return X_p.max()
+
+
+def get_nadir_point(X_p):
+	"""Returns the nadir point of `X_p`, that is the point that minimises each component
+	among the pareto front.
+
+	Args:
+		X_p (pd.DataFrame): Pareto front.
+
+	Returns: (pd.Series) Nadir point.
+	"""
+	return X_p.min()
+
+
+def augmented_tchebycheff_dist(point, ideal_point, nadir_point, epsilon=0.001):
+	"""Computes the augmented tchebycheff distance of a `point` to the ideal point
+	in the direction of the nadir point.
+
+	Args:
+		point (pd.Series)
+		ideal_point (pd.Series)
+		nadir_point (pd.Series)
+		epsilon (float)
+
+	Returns: (float) Augmented Tchebycheff distance.
+	"""
+	norm_i = (ideal_point - point) / (ideal_point - nadir_point)
+	max_norm = norm_i.max()
+	esum = norm_i.sum() * epsilon
+	return max_norm + esum
+
+
+def get_mindist_point(X, ideal_point, nadir_point):
+	"""
+	"""
+	min_idx = 0
+	min_dist = math.inf
+	for i in range(len(X)):
+		if augmented_tchebycheff_dist(X.iloc[i], ideal_point, nadir_point) < min_dist:
+			min_idx = i
+
+	return min_idx
